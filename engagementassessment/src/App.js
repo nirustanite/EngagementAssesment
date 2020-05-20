5e757df77898899daef99c673703e6df98677214
nirupamaa jaya <nirustanite@gmail.com>
2020-05-20 07:44:28 +0200

    add footer and header

--- engagementassessment/src/App.js
+++ engagementassessment/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { HomePage, Component404 } from './components';
+import { HomePage, Component404, Header, Footer } from './components';
+import './App.css';
  
 export default class App extends Component{
     render(){
         return(
             <React.Fragment>
-                
+                 <div className="container">
+                    <Header />
                     <Switch>
                         <Route exact path="/" component={HomePage} />
                         <Route component={Component404} />
                     </Switch>
-               
-                
+                    <Footer />
+                </div>
             </React.Fragment>
         )
     }
